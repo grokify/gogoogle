09b6dca1deac29265d486f062eb8c0e977c289e3
John Wang <johncwang@gmail.com>
2025-01-07 20:43:32 -0800

    feat: `gmailutil`: add `GmailService.Send()`

--- gmailutil/v1/service.go
+++ gmailutil/v1/service.go
@@ -2,15 +2,15 @@ package gmailutil
 
 import (
 	"context"
+	"encoding/base64"
 	"errors"
 	"net/http"
 
+	"github.com/grokify/mogo/net/mailutil"
 	gmail "google.golang.org/api/gmail/v1"
 	"google.golang.org/api/googleapi"
 	"google.golang.org/api/option"
 )
-
-const UserIDMe = "me"
 
 var (
 	ErrGmailServiceCannotBeNil  = errors.New("gmail service cannot be nil")
@@ -37,16 +37,25 @@ func NewGmailService(client *http.Client) (*GmailService, error) {
 	gs := &GmailService{
 		httpClient:     client,
 		APICallOptions: []googleapi.CallOption{}}
-	service, err := gmail.NewService(context.Background(), option.WithHTTPClient(client))
-	if err != nil {
-		return gs, err
+	if service, err := gmail.NewService(context.Background(), option.WithHTTPClient(client)); err != nil {
+		return nil, err
+	} else {
+		gs.Service = service
 	}
-	gs.Service = service
-	gs.UsersService = gmail.NewUsersService(service)
+	gs.UsersService = gmail.NewUsersService(gs.Service)
 	gs.MessagesAPI = MessagesAPI{GmailService: gs}
 	return gs, nil
 }
 
 type MessagesAPI struct {
 	GmailService *GmailService
+}
+
+// Send is a helpfer for https://pkg.go.dev/google.golang.org/api/gmail/v1#UsersMessagesService.Send
+func (gs GmailService) Send(ctx context.Context, from string, msg mailutil.MessageWriter, opts ...googleapi.CallOption) (*gmail.Message, error) {
+	gmsg := &gmail.Message{
+		Raw: base64.URLEncoding.EncodeToString([]byte(msg.String()))}
+	call := gs.UsersService.Messages.Send(from, gmsg)
+	call = call.Context(ctx)
+	return call.Do(opts...)
 }
