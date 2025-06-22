c16be6a4515fb9d5e738b41e3b02ec63691815db
John Wang <johncwang@gmail.com>
2025-06-22 13:16:27 -0700

    refactor: `sheetsutil/iwark`: add `ReadSpreadsheetOpts{}`, `DefaultReadSheetOpts()`

--- sheetsutil/iwark/parse.go
+++ sheetsutil/iwark/parse.go
@@ -18,35 +18,51 @@ var (
 	ErrSheetCannotBeNil = errors.New("sheet cannot be nil")
 )
 
-func ReadTableFromCredentialsSetFile(ctx context.Context, credsFile, credsKey string, sheetID string, sheetIdx uint, headerRows uint32) (*table.Table, error) {
+func ReadTableFromCredentialsSetFile(ctx context.Context, credsFile, credsKey string, sheetID string, sheetIdx uint, opts *ReadSpreadsheetOpts) (*table.Table, error) {
 	if creds, err := goauth.ReadCredentialsFromSetFile(credsFile, credsKey, true); err != nil {
 		return nil, err
 	} else {
-		return ReadTableFromCredentials(ctx, creds, sheetID, sheetIdx, headerRows)
+		return ReadTableFromCredentials(ctx, creds, sheetID, sheetIdx, opts)
 	}
 }
 
-func ReadTableFromCredentialsSet(ctx context.Context, credsSet *goauth.CredentialsSet, credsKey string, sheetID string, sheetIdx uint, headerRows uint32) (*table.Table, error) {
+func ReadTableFromCredentialsSet(ctx context.Context, credsSet *goauth.CredentialsSet, credsKey string, sheetID string, sheetIdx uint, opts *ReadSpreadsheetOpts) (*table.Table, error) {
 	if creds, err := credsSet.Get(credsKey); err != nil {
 		return nil, err
 	} else {
-		return ReadTableFromCredentials(ctx, creds, sheetID, sheetIdx, headerRows)
+		return ReadTableFromCredentials(ctx, creds, sheetID, sheetIdx, opts)
 	}
 }
 
-func ReadTableFromCredentials(ctx context.Context, creds goauth.Credentials, sheetID string, sheetIdx uint, headerRows uint32) (*table.Table, error) {
+type ReadSpreadsheetOpts struct {
+	InclHidden           bool
+	SheetColumnsRowIndex int
+	SheetHeaderRowCount  uint32
+	SheetSkipBody        bool
+}
+
+func DefaultReadSheetOpts() *ReadSpreadsheetOpts {
+	return &ReadSpreadsheetOpts{
+		InclHidden:           false,
+		SheetColumnsRowIndex: 0,
+		SheetHeaderRowCount:  1,
+		SheetSkipBody:        true,
+	}
+}
+
+func ReadTableFromCredentials(ctx context.Context, creds goauth.Credentials, sheetID string, sheetIdx uint, opts *ReadSpreadsheetOpts) (*table.Table, error) {
 	if clt, err := creds.NewClient(ctx); err != nil {
 		return nil, err
 	} else {
-		return ReadTableFromClient(clt, sheetID, 0, 1)
+		return ReadTableFromClient(clt, sheetID, sheetIdx, opts)
 	}
 }
 
-func ReadTableFromClient(client *http.Client, sheetID string, sheetIdx uint, headerRows uint32) (*table.Table, error) {
+func ReadTableFromClient(client *http.Client, sheetID string, sheetIdx uint, opts *ReadSpreadsheetOpts) (*table.Table, error) {
 	if ss, err := ReadSpreadsheetFromClient(client, sheetID); err != nil {
 		return nil, err
 	} else {
-		return ParseTableFromSpreadsheet(*ss, sheetIdx, headerRows)
+		return ParseTableFromSpreadsheet(*ss, sheetIdx, opts)
 	}
 }
 
@@ -87,26 +103,30 @@ func ReadSpreadsheetFromClient(client *http.Client, sheetID string) (*spreadshee
 	}
 }
 
-func ParseTableFromSpreadsheet(ss spreadsheet.Spreadsheet, sheetIdx uint, headerRows uint32) (*table.Table, error) {
+func ParseTableFromSpreadsheet(ss spreadsheet.Spreadsheet, sheetIdx uint, opts *ReadSpreadsheetOpts) (*table.Table, error) {
 	if s, err := ss.SheetByIndex(sheetIdx); err != nil {
 		return nil, err
 	} else {
-		return ParseTableFromSheet(s, headerRows)
+		return ParseTableFromSheet(s, opts)
 	}
 }
 
-func ParseTableFromSheet(s *spreadsheet.Sheet, headerRows uint32) (*table.Table, error) {
+func ParseTableFromSheet(s *spreadsheet.Sheet, opts *ReadSpreadsheetOpts) (*table.Table, error) {
 	if s == nil {
 		return nil, ErrSheetCannotBeNil
 	}
-	cols, rows := ParseDataFromSheet(s, headerRows)
+	cols, rows := ParseDataFromSheet(s, opts)
 	tbl := table.NewTable("")
+	tbl.Name = s.Properties.Title
 	tbl.Columns = cols
 	tbl.Rows = rows
 	return &tbl, nil
 }
 
-func ParseDataFromSheet(s *spreadsheet.Sheet, headerRows uint32) ([]string, [][]string) {
+func ParseDataFromSheet(s *spreadsheet.Sheet, opts *ReadSpreadsheetOpts) ([]string, [][]string) {
+	if opts == nil {
+		opts = DefaultReadSheetOpts()
+	}
 	var cols []string
 	var rows [][]string
 	for i, srow := range s.Rows {
@@ -114,11 +134,11 @@ func ParseDataFromSheet(s *spreadsheet.Sheet, headerRows uint32) ([]string, [][]
 		for _, scell := range srow {
 			row = append(row, scell.Value)
 		}
-		if headerRows > 0 && i < int(headerRows) {
-			if i == 0 {
-				cols = row
-			}
-		} else {
+		if i == opts.SheetColumnsRowIndex {
+			cols = row
+		} else if opts.SheetHeaderRowCount > 0 && i < int(opts.SheetHeaderRowCount) {
+			continue
+		} else if !opts.SheetSkipBody {
 			rows = append(rows, row)
 		}
 	}
